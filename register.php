66a9f109db5316cdbda42c0467518043ef431f61
Juan Torchia <j.s.torchia@gmail.com>
2020-08-27 23:26:44 -0300

    first

---
+++ register.php
@@ -0,0 +1,155 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>Flatkit - HTML Version | Bootstrap 4 Web App Kit with AngularJS</title>
+  <meta name="description" content="Admin, Dashboard, Bootstrap, Bootstrap 4, Angular, AngularJS" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <!-- for ios 7 style, multi-resolution icon of 152x152 -->
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-barstyle" content="black-translucent">
+  <link rel="apple-touch-icon" href="assets/images/logo.png">
+  <meta name="apple-mobile-web-app-title" content="Flatkit">
+  <!-- for Chrome on Android, multi-resolution icon of 196x196 -->
+  <meta name="mobile-web-app-capable" content="yes">
+  <link rel="shortcut icon" sizes="196x196" href="assets/images/logo.png">
+  
+  <!-- style -->
+  <link rel="stylesheet" href="assets/animate.css/animate.min.css" type="text/css" />
+  <link rel="stylesheet" href="assets/glyphicons/glyphicons.css" type="text/css" />
+  <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css" type="text/css" />
+  <link rel="stylesheet" href="assets/material-design-icons/material-design-icons.css" type="text/css" />
+
+  <link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css" type="text/css" />
+  <!-- build:css ../assets/styles/app.min.css -->
+  <link rel="stylesheet" href="assets/styles/app.css" type="text/css" />
+  <!-- endbuild -->
+  <link rel="stylesheet" href="assets/styles/font.css" type="text/css" />
+</head>
+
+<?php
+
+// Create connection
+$conn = mysqli_connect("localhost", "root", "Mario456luis$", "users");
+
+
+// Declaraciones
+$email = "";
+$password = "";
+$password_r = "";
+$msg = "";
+
+if( isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password_r'])){
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password_r = $_POST['password_r'];
+
+
+    if ($password==$password_r){
+        $result = $conn->query("SELECT * FROM `users` WHERE `user_email` = '$email'");
+        $users = $result -> fetch_all(MYSQLI_ASSOC);
+
+        //mysqli_query($db_connection, "SELECT `user_email` FROM `users` WHERE user_email = '$user_email'");
+
+        $count = count($users);
+
+if ($count == 0){
+  $password = sha1($password);
+  $conn->query("INSERT INTO 'users' ('users_email', 'user_password') VALUES ('".$email."', '".$password."');");
+  $msg.="Usuario creado correctamente, ingresa haciendo click  link";
+  }else{
+  $msg.="El Mail ingresa ya existe";
+  }
+
+  }else{
+      $msg ="Las Claves no coinciden";
+  }
+}else{
+  $msg = "Complete el formulario";
+}
+  ?>
+<body>
+  <div class="app" id="app">
+
+<!-- ############ LAYOUT START-->
+  <div class="center-block w-xxl w-auto-xs p-y-md">
+    <div class="navbar">
+      <div class="pull-center">
+        <div ui-include="'views/blocks/navbar.brand.html'"></div>
+      </div>
+    </div>
+
+    <div class="p-a-md box-color r box-shadow-z1 text-color m-a">
+      <div class="m-b text-sm">
+        Sign up to your Flatkit Account
+      </div>
+
+    
+      <form method="post" target="register.php" name="form">
+        <div class="md-form-group">
+          <input name="email" type="email" class="md-input" value=" <?php echo $email; ?> " required>
+          <label>Email</label>
+        </div>
+        <div class="md-form-group">
+          <input name="password" type="password" class="md-input" required>
+          <label>Password</label>
+        </div>
+        <div class="md-form-group">
+          <input name="password_r" type="password" class="md-input" required>
+          <label>Re Password</label>
+        </div>
+        <div class="m-b-md">
+          <label class="md-check">
+            <input type="checkbox" required><i class="primary"></i> Agree the <a href>terms and policy</a>
+          </label>
+        </div>
+        <button type="submit" class="btn primary btn-block p-x-md">Sign up</button>
+      </form>
+    
+    </div>
+
+    <div class="p-v-lg text-center">
+      <div>Already have an account? <a ui-sref="access.signin" href="signin.html" class="text-primary _600">Sign in</a></div>
+    </div>
+  </div>
+
+<!-- ############ LAYOUT END-->
+
+  </div>
+<!-- build:js scripts/app.html.js -->
+<!-- jQuery -->
+  <script src="libs/jquery/jquery/dist/jquery.js"></script>
+<!-- Bootstrap -->
+  <script src="libs/jquery/tether/dist/js/tether.min.js"></script>
+  <script src="libs/jquery/bootstrap/dist/js/bootstrap.js"></script>
+<!-- core -->
+  <script src="libs/jquery/underscore/underscore-min.js"></script>
+  <script src="libs/jquery/jQuery-Storage-API/jquery.storageapi.min.js"></script>
+  <script src="libs/jquery/PACE/pace.min.js"></script>
+
+  <script src="html/scripts/config.lazyload.js"></script>
+
+  <script src="html/scripts/palette.js"></script>
+  <script src="html/scripts/ui-load.js"></script>
+  <script src="html/scripts/ui-jp.js"></script>
+  <script src="html/scripts/ui-include.js"></script>
+  <script src="html/scripts/ui-device.js"></script>
+  <script src="html/scripts/ui-form.js"></script>
+  <script src="html/scripts/ui-nav.js"></script>
+  <script src="html/scripts/ui-screenfull.js"></script>
+  <script src="html/scripts/ui-scroll-to.js"></script>
+  <script src="html/scripts/ui-toggle-class.js"></script>
+
+  <script src="html/scripts/app.js"></script>
+
+  <!-- ajax -->
+  <script src="libs/jquery/jquery-pjax/jquery.pjax.js"></script>
+  <script src="html/scripts/ajax.js"></script>
+<!-- endbuild -->
+</body>
+</html>
